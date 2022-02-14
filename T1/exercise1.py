6e8e172309442cc91abc26b1dbb36ab5b89c96ef
hanon11 <lucia.atienza2@gmail.com>
2022-02-14 10:58:03 +0100

    T1

---
+++ T1/exercise1.py
@@ -0,0 +1,3 @@
+# Design an FA that accepts any binary string of lenght 4. Input
+# will be a binary string starting with most significant digit. Length will have an undefined 
+# length and can start with zeros.
