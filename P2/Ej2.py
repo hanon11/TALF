25ff9f978548dac7026feecc5c1fd1046e2dbb2e
hanon11 <lucia.atienza2@gmail.com>
2022-03-09 21:31:59 +0100

    'P2'

--- P2/Ej2.py
+++ P2/Ej2.py
@@ -1,4 +1,4 @@
-quijote = open('ElQuijote.txt','r') 
+quijote = open('ElQuijote.txt', 'r')
 lista = []
 for linea in quijote:
     frase = linea.split()
@@ -8,7 +8,7 @@ lista_lower = []
 for elemento in lista:
     lista_lower.append(elemento.lower())
 
-alfabeto = ['a', 'd', 'l', 'h', 'i', 'g' , 'o'] 
+alfabeto = ['a', 'd', 'l', 'h', 'i', 'g', 'o']
 contiene = False
 final = []
 for elemento in lista_lower:
@@ -16,8 +16,8 @@ for elemento in lista_lower:
     for letra in elemento:
         if letra not in alfabeto:
             contiene = True
-    if contiene == False:
+    if not contiene:
         final.append(elemento)
-convert_to_set = set(final) # sin repeticiones
+convert_to_set = set(final)  # sin repeticiones
 print(convert_to_set)
 quijote.close()
